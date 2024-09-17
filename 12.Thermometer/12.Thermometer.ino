de569b2c0131f4cbcda431550c37036549466808
ramup052 <pillalamarriramu505@gmail.com>
2024-09-17 12:07:54 +0530

    Add files via upload

---
+++ 12.Thermometer/12.Thermometer.ino
@@ -0,0 +1,16 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int t=analogRead(A0);
+  float R=(float)t/1023;
+  float temp=R*500;
+  Serial.println(temp);
+
+}
